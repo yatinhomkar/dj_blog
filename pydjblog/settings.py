1b2e03b136f0c8c946c7d47013964fb0197ac0cb
Yatin <yatinhomkar@gmail.com>
2020-01-21 23:24:30 +0530

    updated settings.py with django_heroku import and updated requirements.txt

--- pydjblog/settings.py
+++ pydjblog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -144,3 +145,8 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'pragyat2019@gmail.com'
 EMAIL_HOST_PASSWORD ='test@1019'
+
+
+
+django_heroku.settings(locals())  
+
